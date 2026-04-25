ac15013564da27b4d5b4fe04091670eea6b4c544
Luis Giuliano <luisgiuliano255@icloud.com>
2026-04-25 02:06:18 +0200

    ui: add ScrollToTop to fix navigation scroll position

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
@@ -8,9 +8,19 @@ import TermsOfService from "./pages/TermsOfService";
 import TermsOfServiceDE from "./pages/TermsOfServiceDE";
 import Impressum from "./pages/Impressum";
 
+// Hilfskomponente, die bei jedem Routenwechsel nach oben scrollt
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+};
+
 const App: React.FC = () => {
     return (
         <Router>
+            <ScrollToTop />
             <Layout>
                 <Routes>
                     <Route path="/" element={<Home />} />
