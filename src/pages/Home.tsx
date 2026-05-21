cf52d4311e1fd496a73b59eb8ea0562b61960b65
Luis Giuliano <luisgiuliano255@icloud.com>
2026-05-21 14:24:02 +0200

    feat: render hero section with waitlist on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,14 @@
+import HeroSection from '../components/v3/HeroSection';
 import StickyScrollShowcase from '../components/v3/StickyScrollShowcase';
 
 const Home = () => {
   return (
-    <main className="bg-[#05000a] min-h-screen">
-      {/* Falls du noch eine Hero-Sektion (mit dem Waitlist-Button) hast, 
-        kannst du sie hier über der Animation einfügen!
-      */}
+    <main className="bg-white dark:bg-[#05000a] min-h-screen transition-colors duration-500">
       
-      {/* Das neue V3 Herzstück */}
+      {/* Der neue KI-Startup Hero-Bereich mit der Waitlist */}
+      <HeroSection />
+      
+      {/* Das neue V3 Herzstück (4-Bilder) */}
       <StickyScrollShowcase />
       
     </main>
